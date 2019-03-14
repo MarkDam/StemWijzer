41277b2e5431d073a3affbad4ae7bae265c9ca4b
MarkDam <31473027+MarkDam@users.noreply.github.com>
2019-03-14 17:57:43 +0100

    Add files via upload

--- testLogic.js
+++ testLogic.js
@@ -1,38 +1,25 @@
 var outcome = [];
 var questionsAnswered = 0;
 var list = document.getElementById('list');
+var mainId = document.getElementsByClassName('main');
 
 document.getElementById('title').innerHTML = subjects[questionsAnswered].title;
 document.getElementById('statement').innerHTML = subjects[questionsAnswered].statement;
 
 
 
-function pro() {
+function answer(antwoord) {
 	endScreen();
-	outcome.push("pro");
+	outcome.push(antwoord);
 	questions();
-	/*
+/*
 	for (let i = 0; i < parties.length; i++) {
-	if (subjects[questionsAnswered].parties.position == pro) {
-			results.score++;
+	if (subjects[questionsAnswered].parties[i].position == pro) {
+		results[i].score ++;
+	//	results[i] = parseInt(results[i]+1);
 		}
 	}
-	*/
-}
-function contra() {
-	endScreen();
-	outcome.push("contra");
-	questions();
-}
-function neither() {
-	endScreen();
-  outcome.push("neither");
-	questions();
-}
-function skip() {
-	endScreen();
-	outcome.push("skip");
-	questions();
+*/
 }
 
 function endScreen() {
@@ -43,7 +30,7 @@ function endScreen() {
 			document.getElementById('bigPartiesButton').style.visibility = "visible";
 			document.getElementById('secularPartiesButton').style.visibility = "visible";
 			document.getElementById('allPartiesButton').style.visibility = "visible";
-			var mainId = document.getElementsByClassName('main');
+			document.getElementById('goToOutcome').style.visibility = "visible";
 			mainId[0].style.height = "700px";
 			mainId[0].style.width = "470px";
 //			var bigParties = document.createElement('BUTTON');
@@ -69,22 +56,49 @@ function goBack() {
 	     } else {
          questionsAnswered--;
 	        document.getElementById('title').innerHTML = subjects[questionsAnswered].title;
-	         document.getElementById('statement').innerHTML = subjects[questionsAnswered].statement;
-					 outcome.pop();
+	        document.getElementById('statement').innerHTML = subjects[questionsAnswered].statement;
+					outcome.pop();
+		 			document.getElementById('voteButtons').style.visibility = "visible";
+		 			document.getElementById('statement').style.visibility = "visible";
+		 			document.getElementById('bigPartiesButton').style.visibility = "hidden";
+		 			document.getElementById('secularPartiesButton').style.visibility = "hidden";
+		 			document.getElementById('allPartiesButton').style.visibility = "hidden";
+					document.getElementById('list').style.visibility = "hidden";
+					document.getElementById('goToOutcome').style.visibility = "hidden";
+					list.innerHTML = "";
+					mainId[0].style.height = "470px";
+					mainId[0].style.width = "1000px";
          }
 }
 
+function theOutcome() {
+	document.getElementById('title').innerHTML = "Uw beste partijen zijn:";
+	document.getElementById('voteButtons').style.visibility = "hidden";
+	document.getElementById('statement').style.visibility = "hidden";
+	document.getElementById('bigPartiesButton').style.visibility = "hidden";
+	document.getElementById('secularPartiesButton').style.visibility = "hidden";
+	document.getElementById('allPartiesButton').style.visibility = "hidden";
+	document.getElementById('list').style.visibility = "hidden";
+	document.getElementById('goToOutcome').style.visibility = "hidden";
+
+	list.innerHTML = "";
+
+	mainId[0].style.height = "470px";
+	mainId[0].style.width = "1000px";
+}
+
 function sortBigParties() {
+	list.innerHTML = "";
+
 	var bigParties = [];
 	for (let i = 0; i < parties.length; i++) {
 		if (parties[i].size >= 1) {
 			bigParties.push(parties[i].name);
 		}
 	}
-	list.innerHTML = "";
 	for (let i = 0; i < bigParties.length; i++) {
 		var partiesList = document.createElement('li');
-		partiesList.innerText = parties[i].name;
+		partiesList.innerText = bigParties[i];
 		list.appendChild(partiesList);
 	}
 }
@@ -93,13 +107,13 @@ function sortSecularParties() {
 
 	var secularParties = [];
 	for (var i = 0; i < parties.length; i++) {
-		if (parties[i].secular == true) {
+		if (parties[i].secular === true) {
 			secularParties.push(parties[i].name);
 		}
 	}
 	for (let i = 0; i < secularParties.length; i++) {
 		var partiesList = document.createElement('li');
-		partiesList.innerText = parties[i].name;
+		partiesList.innerText = secularParties[i];
 		list.appendChild(partiesList);
 	}
 }
